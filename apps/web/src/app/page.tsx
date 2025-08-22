3a52799ab84e398a76b3e4a88297503536e69ee4
Rostislav Nagimov <rostislav.nagimov@gmail.com>
2025-08-22 15:39:57 +0500

    feat: add sample structure

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -4,7 +4,26 @@ export default function Home() {
   return (
     <main>
       <h1>Hello NanoUI</h1>
-      <Button>Click me</Button>
+
+      <section className="example-section">
+        <h2>Button example</h2>
+        <Button>Click me</Button>
+      </section>
+      <section className="example-section">
+        <h2>Switch example</h2>
+      </section>
+      <section className="example-section">
+        <h2>Tooltip example</h2>
+      </section>
+      <section className="example-section">
+        <h2>File Uploader example</h2>
+      </section>
+      <section className="example-section">
+        <h2>List example</h2>
+      </section>
+      <section className="example-section">
+        <h2>Progress example</h2>
+      </section>
     </main>
   )
 }
