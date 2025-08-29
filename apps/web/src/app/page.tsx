ba5c1ae6bd0ff0818942973f7acf3bcf867e4028
muratovnur <muratovv.nurbol@gmail.com>
2025-08-29 20:03:08 +0500

    feat: add component Button

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,28 +1,119 @@
+'use client'
+
 import {Button} from '@nanoui/core'
+import {useEffect, useState} from 'react'
+import SuccessIconSVG from './icons/SuccessIconSVG'
+import ErrorIconSVG from './icons/ErrorIconSvg'
+
+type TSize = 'sm' | 'md' | 'lg'
 
 export default function Home() {
+  const [size, setSize] = useState<TSize>('md')
+
+  const handleSizeChange = (size: TSize) => {
+    setSize(size)
+  }
+
+  const getExample = () => {
+    return fetch('https://jsonplaceholder.typicode.com/todo/1')
+      .then((response) => {
+        if (!response.ok) throw Error(response.statusText)
+        return response.json()
+      })
+      .then((json) => {
+        return json
+      })
+  }
+
   return (
     <main>
       <h1>Hello NanoUI</h1>
-
+      <div className="controller">
+        <span className="">Size: {size}</span>
+        <Button
+          onClick={() => {
+            handleSizeChange('sm')
+          }}
+        >
+          Small
+        </Button>
+        <Button
+          onClick={() => {
+            handleSizeChange('md')
+          }}
+        >
+          Medium
+        </Button>
+        <Button
+          onClick={() => {
+            handleSizeChange('lg')
+          }}
+        >
+          Large
+        </Button>
+      </div>
       <section className="example-section">
         <h2>Button example</h2>
-        <Button>Click me</Button>
-      </section>
-      <section className="example-section">
-        <h2>Switch example</h2>
-      </section>
-      <section className="example-section">
-        <h2>Tooltip example</h2>
-      </section>
-      <section className="example-section">
-        <h2>File Uploader example</h2>
-      </section>
-      <section className="example-section">
-        <h2>List example</h2>
-      </section>
-      <section className="example-section">
-        <h2>Progress example</h2>
+        <div className="example-section-item">
+          <span className="example-section-name">Auto Resolve State = true:</span>
+          <Button
+            size={size}
+            onClick={getExample}
+            autoResolveState={true}
+            successIcon={<SuccessIconSVG />}
+            errorIcon={<ErrorIconSVG />}
+          >
+            Click me
+          </Button>
+        </div>
+        <div className="example-section-item">
+          <span className="example-section-name">Loading:</span>
+          <Button size={size} loading={true}>
+            Click me
+          </Button>
+        </div>
+        <div className="example-section-item">
+          <span className="example-section-name">Disabled:</span>
+          <Button size={size} disabled>
+            Click me
+          </Button>
+        </div>
+        <div className="example-section-item">
+          <span className="example-section-name">Primary:</span>
+          <Button size={size} variant="primary">
+            Click me
+          </Button>
+        </div>
+        <div className="example-section-item">
+          <span className="example-section-name">Secondary:</span>
+          <Button size={size} variant="secondary">
+            Click me
+          </Button>
+        </div>
+        <div className="example-section-item">
+          <span className="example-section-name">Outline:</span>
+          <Button size={size} variant="outline">
+            Click me
+          </Button>
+        </div>
+        <div className="example-section-item">
+          <span className="example-section-name">Ghost:</span>
+          <Button size={size} variant="ghost">
+            Click me
+          </Button>
+        </div>
+        <div className="example-section-item">
+          <span className="example-section-name">Link:</span>
+          <Button size={size} variant="link">
+            Click me
+          </Button>
+        </div>
+        <div className="example-section-item">
+          <span className="example-section-name">As anchor tag:</span>
+          <Button size={size} variant="link" as="a">
+            Click me
+          </Button>
+        </div>
       </section>
     </main>
   )
--- packages/nanoui/src/components/button/Button.tsx
+++ packages/nanoui/src/components/button/Button.tsx
@@ -1,10 +1,145 @@
-import React from 'react'
+import React, {useEffect, useRef, useState} from 'react'
 import styles from './Button.module.css'
 
 type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
-  variant?: 'primary' | 'secondary'
+  variant?: 'primary' | 'secondary' | 'outline' | 'ghost' | 'link'
+  size?: 'sm' | 'md' | 'lg'
+  disabled?: boolean
+  loading?: boolean
+  autoResolveState?: boolean
+  successIcon?: React.ReactNode
+  errorIcon?: React.ReactNode
+  loaderIcon?: React.ReactNode
+  icon?: React.ReactNode
+  iconPosition?: 'left' | 'right'
+  onClick?: (e: MouseEvent) => Promise<any> | void
+  onMouseDown?: (e: MouseEvent) => void
+  onTouchStart?: (e: TouchEvent) => void
+  as?: keyof React.JSX.IntrinsicElements | React.ComponentType
+  className?: string
+  style?: React.CSSProperties
+  href?: string
+  children: React.ReactNode
 }
 
-export const Button: React.FC<ButtonProps> = ({variant = 'primary', ...props}) => {
-  return <button className={styles[variant]} {...props} />
+export const Button: React.FC<ButtonProps> = ({
+  variant = 'primary',
+  size = 'md',
+  disabled = false,
+  loading = false,
+  autoResolveState = false,
+  successIcon,
+  errorIcon,
+  loaderIcon,
+  icon,
+  iconPosition = 'left',
+  onClick,
+  onMouseDown,
+  onTouchStart,
+  as: Component = 'button',
+  className,
+  style,
+  href = '#',
+  children,
+  ...props
+}) => {
+  const [isLoading, setIsLoading] = useState(false)
+  const [isSuccess, setIsSuccess] = useState(false)
+  const [isError, setIsError] = useState(false)
+  const timeoutId = useRef<number | null>(null)
+
+  //TODO: handle type, change any
+  const handleInteraction = (callback: ((e: any) => Promise<any> | void) | undefined, ...args: any) => {
+    if (!callback) return
+
+    if (autoResolveState) handleAutoResolve(callback)
+    else callback(args.e)
+  }
+
+  const handleClearTimeout = () => {
+    if (timeoutId.current !== null) {
+      clearTimeout(timeoutId.current)
+      timeoutId.current = null
+    }
+  }
+
+  //TODO: handle type, change any
+  const handleAutoResolve = (fn: (...args: any) => any) => {
+    handleClearTimeout()
+    setIsLoading(true)
+
+    fn()
+      .then((response: any) => {
+        setIsSuccess(true)
+      })
+      .catch(() => {
+        setIsError(true)
+      })
+      .finally(() => {
+        timeoutId.current = setTimeout(() => {
+          setIsLoading(false)
+          setIsSuccess(false)
+          setIsError(false)
+        }, 2000)
+      })
+  }
+
+  const buttonClassNames = () => {
+    let returnClassName = `${styles['button-base']} ${styles[`button-${variant}`]} ${styles[`button-${size}`]}`
+    if (disabled) returnClassName += ` ${styles[`button-disabled`]}`
+
+    return className ? returnClassName + className : returnClassName
+  }
+
+  const ownProps = () => {
+    //TODO: handle type, change any
+    let returnProps: any = {
+      onClick: (e: MouseEvent) => handleInteraction(onClick, e),
+      onMouseDown: (e: MouseEvent) => handleInteraction(onMouseDown, e),
+      onTouchStart: (e: TouchEvent) => handleInteraction(onTouchStart, e),
+      disabled: disabled || loading || isLoading,
+      ...props
+    }
+
+    if (Component !== 'button') {
+      returnProps.role = 'button'
+      returnProps.href = href
+    }
+    if (disabled || loading || isLoading) returnProps['aria-disabled'] = true
+
+    return returnProps
+  }
+
+  const stateHandler = (
+    state: boolean | React.ReactNode | undefined,
+    icon: React.ReactNode,
+    fallback?: React.ReactNode
+  ) => {
+    if (!state || (!icon && !fallback)) return null
+    return (
+      <div className={styles['button-state-wrapper']}>
+        <div className={styles['button-state']}>{icon || fallback}</div>
+      </div>
+    )
+  }
+
+  useEffect(() => {
+    return () => {
+      handleClearTimeout()
+    }
+  }, [])
+
+  return (
+    <Component className={buttonClassNames()} style={style} {...ownProps()}>
+      <>
+        {isSuccess && stateHandler(isSuccess, successIcon, <span>Success</span>)}
+        {isError && stateHandler(isError, errorIcon, <span>Error</span>)}
+        {(!isSuccess || !isError || loading || isLoading) &&
+          stateHandler(isSuccess, loaderIcon, <div className={styles['button-loader']} />)}
+        {icon && iconPosition === 'left' && icon}
+        {children}
+        {icon && iconPosition !== 'left' && icon}
+      </>
+    </Component>
+  )
 }
