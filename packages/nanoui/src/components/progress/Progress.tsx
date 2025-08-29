9d73cd0aaf2188740531eb07a77620ec9e8c6824
rekhab <itsrekhab@gmail.com>
2025-08-29 10:33:49 +0500

    feat: add round borders and change aria-label language in Progress component

--- packages/nanoui/src/components/progress/Progress.tsx
+++ packages/nanoui/src/components/progress/Progress.tsx
@@ -1,30 +1,30 @@
-import { type FC } from 'react'
+import {type FC} from 'react'
 import styles from './Progress.module.css'
 
 interface ProgressProps {
-  value?: number;
-  max?: number;
-  size?: 'sm' | 'md' | 'lg';
-  className?: string;
-  style?: React.CSSProperties;
+  value?: number
+  max?: number
+  size?: 'sm' | 'md' | 'lg'
+  className?: string
+  style?: React.CSSProperties
 }
 
-const Progress: FC<ProgressProps> = ({ value, max = 100, size = 'md', className, style }) => {
-  const allStyles: Array<string | undefined> = [styles['progress'], size ? styles[size] : undefined, className];
-  const classNames: string = allStyles.filter(v => v !== undefined).join(" ");
-  const width: string = value ? Math.min(Math.floor(value / max * 100), 100) + "%" : "0";
+const Progress: FC<ProgressProps> = ({value, max = 100, size = 'md', className, style}) => {
+  const allStyles: Array<string | undefined> = [styles['progress'], size ? styles[size] : undefined, className]
+  const classNames: string = allStyles.filter((v) => v !== undefined).join(' ')
+  const width: string = value ? Math.min(Math.floor((value / max) * 100), 100) + '%' : '0'
 
   return (
     <div
       className={classNames}
-      style={{ ...style, ...{ '--progress-value': width } }}
+      style={{...style, ...{'--progress-value': width}}}
       role="progressbar"
       aria-valuenow={value}
       aria-valuemin={value ? 0 : undefined}
       aria-valuemax={value ? max : undefined}
-      aria-label={`Прогресс: ${ value ? width : 'не задан'}`}
+      aria-label={`Progress: ${value ? width : 'not set'}`}
     />
-  );
+  )
 }
 
-export default Progress;
+export default Progress
