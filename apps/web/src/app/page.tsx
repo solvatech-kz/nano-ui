c7fc89c1d6f4526d8f6c3ed8aa62bf59d5494e3f
rekhab <73596345+itsrekhab@users.noreply.github.com>
2025-08-29 14:09:44 +0500

    component: Progress (#7)
    * feat: add component Progress
    
    * style: removed comments
    
    * refactor: removed redundant ::before element
    
    * feat: added interactive Progress component preview
    
    * feat: add round borders and change aria-label language in Progress component
    
    * refactor: change Progress component export method
    
    * style: format
    
    * refactor(css): shorten css values
    
    * refactor: change Progress and ProgressPreview exports

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,5 +1,6 @@
 import {Button} from '@nanoui/core'
 import {Switch} from '@nanoui/core/src/components/switch'
+import {ProgressPreview} from '@/components/ProgressPreview'
 
 export default function Home() {
   return (
@@ -25,6 +26,7 @@ export default function Home() {
       </section>
       <section className="example-section">
         <h2>Progress example</h2>
+        <ProgressPreview />
       </section>
     </main>
   )
