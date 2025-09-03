f7b61a301944003f7209b98e8f4e3e413ebe9f39
Daniya <d17810049@gmail.com>
2025-09-03 15:58:24 +0500

    style: исправлена стилизация в List.tsx

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -3,11 +3,14 @@ import {ProgressPreview} from '@/components/ProgressPreview'
 
 export default function Home() {
   const items = [
-    { id: 1, icon: <img src="/vercel.svg" alt="Icon" />, text: "Элемент с иконкой" },
-    { id: 2, text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur vel sem sed nulla scelerisque fermentum. Integer non libero ut arcu pharetra mattis in nec velit. Nulla facilisi. Donec bibendum eros sit amet lectusmollis, at dignissim odio hendrerit", multiline: false },
-    { id: 3, text: <strong>Можно даже React-элемент</strong> },
-  ];
-
+    {id: 1, icon: <img src="/vercel.svg" alt="Icon" />, text: 'Элемент с иконкой'},
+    {
+      id: 2,
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur vel sem sed nulla scelerisque fermentum. Integer non libero ut arcu pharetra mattis in nec velit. Nulla facilisi. Donec bibendum eros sit amet lectusmollis, at dignissim odio hendrerit',
+      multiline: false
+    },
+    {id: 3, text: <strong>Можно даже React-элемент</strong>}
+  ]
 
   return (
     <main>
@@ -27,7 +30,7 @@ export default function Home() {
       </section>
       <section className="example-section">
         <h2>List example</h2>
-        <List items={items} ordered={false} style={{ width: '500px'}}/>
+        <List items={items} ordered={false} />
       </section>
       <section className="example-section">
         <h2>Progress example</h2>
--- packages/nanoui/src/components/list/List.tsx
+++ packages/nanoui/src/components/list/List.tsx
@@ -1,37 +1,33 @@
-import type { CSSProperties, FC, ReactNode } from "react";
-import styles from './List.module.css';
+import type {CSSProperties, FC, ReactNode} from 'react'
+import styles from './List.module.css'
 
 interface ListItem {
-  id: string | number;
-  text: string | ReactNode;
-  icon?: ReactNode;
-  multiline?: boolean;
+  id: string | number
+  text: string | ReactNode
+  icon?: ReactNode
+  multiline?: boolean
 }
 
 interface ListProps {
-  items: ListItem[];
-  ordered?: boolean;
-  className?: string;
-  style?: CSSProperties;
+  items: ListItem[]
+  ordered?: boolean
+  className?: string
+  style?: CSSProperties
 }
 
-export const List :FC<ListProps> = ({items, ordered = true, className, style}) => {
-  const ListTag = ordered ? "ol" : "ul";
-return (
-  <ListTag className={`${className || ""} ${styles.list} ${!ordered ? styles.unordered : ""}`} style={style}>
-    {items.map(({id, icon, text, multiline}) => (
-      <li key={id} className={`${styles.item} ${multiline ? styles.multiline : styles.singleLine}`}>
-        <div className={styles.content}>
-          {icon &&
-            <span className={styles.icon}>
-          {icon}
-        </span>}
-          <span className={styles.text}>
-          {text}
-        </span>
-        </div>
-      </li>
-    ))}
-  </ListTag>
-);
-};
+export const List: FC<ListProps> = ({items, ordered = true, className, style}) => {
+  const ListTag = ordered ? 'ol' : 'ul'
+  return (
+    <ListTag className={`${className || ''} ${styles.list} ${!ordered ? styles.unordered : ''}`} style={style}>
+      {items.map(({id, icon, text, multiline}) => (
+        <li key={id} className={`${styles.item} ${multiline ? styles.multiline : styles.singleLine}`}>
+          <div className={styles.content}>
+            {icon && <span className={styles.icon}>{icon}</span>}
+            <span className={styles.text}>{text}</span>
+          </div>
+          <hr className={styles.separator} />
+        </li>
+      ))}
+    </ListTag>
+  )
+}
